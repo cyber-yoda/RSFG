38709f02e9365338348a74cce00ff8473e6eafb3
cacphony2000 <61602730+cyber-yoda@users.noreply.github.com>
2025-07-14 10:54:20 -0700

    Updated syntax and rename Div to div.sh
    Refer to last commit regarding majority of changes; syntax changes were merely due to the unreliable/unpredictable behavior that is exhibited by that gremlin called 'echo'

---
+++ RSFG 0.9.7/div.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+#
+# Name: Div.sh
+# Project: RSFG
+# Description: Just a basic bash shell script showing what printed output may be like.
+#
+
+printf
+printf '%s' "----"
+printf
