e3dd6b55cc90f3e43d32dd3aba3f4775ea9ad674
cacphony2000 <61602730+cyber-yoda@users.noreply.github.com>
2025-07-14 13:17:13 -0700

    Update and rename randomGen to random_gen.sh
    use correct naming convention for bash scripts and correct a few other things in this

--- RSFG 0.9.7/random_gen.sh
+++ RSFG 0.9.7/random_gen.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# Author: Cyber-Yoda
-# Generates a random string using awk and sed, without echo
-# Project: RSFG-rewrite
-# Description: Factor of majority
+#
+# Name: random_gen.sh
+# Project: RSFG
+# Description: Factor of majority and generate a random string using awk, sed, and tr
+#
 
 generator() {
   # lists and arrays
