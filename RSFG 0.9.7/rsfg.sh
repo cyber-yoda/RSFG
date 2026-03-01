6607ee20e53662c480d8abb35f4fdcb5bcebe429
cacphony2000 <61602730+cyber-yoda@users.noreply.github.com>
2026-03-01 06:37:04 -0800

    Update rsfg.sh to be more dynamic part 1
    This update is eliminating excessive `if-else` and `echo` usage while also condensing that logic into functions and `awk` calls.
    
    exec_mod is sort of a dynamic execution engine here

--- RSFG 0.9.7/rsfg.sh
+++ RSFG 0.9.7/rsfg.sh
@@ -1,71 +1,36 @@
-#!/usr/bin/bash
-# a silly script that, creates a garbage file, cleans the file, then sorts the output
-#
+#!/usr/bin/env bash
+# Internal System Maintenance Utility (Legacy)
 
-#./DeleteUtil
+printf "Mode (S/V) > " && read x
+awk -v val="${x^^}" 'BEGIN { print val > "TempFiles/Value.txt") }'
 
-printf "Enter S for Silent Mode, enter  V for Verbose Mode: "
-read x
+exec_mod() {
+    if [[ "$x" =~ [Vv] ]]; then
+        printf "\n[*] Initializing %s... \n" "$1"
+        local timeout=4
+        while [ $timeout -gt 0 ]; do
+            : # No-op colon is a bash built-in that does nothing
+            ((timeout--))
+            read -t 5 -n 0 2>/dev/null # Silent 5 second interval
+        done
+    fi
 
-#rm -v TempFiles/Value.txt
-printf "%s\n" $x > TempFiles/Value.txt #Submitting User input to a file to be read back by the other sub programs. This will set them into either silent or verbose mode.
-	
-if [ $x = 'S' ] || [ $x = 's' ]; then #If the user entered S, then it will run the all the sub programs in silent mode.
-	printf
-	./GarbageGen #Runs the GarbageGen module, creates a garbage file.
-	sleep 1
-	./FileCleaner #Runs the FileCleaner module, cleans the file and preps it for the sorting module.
-	sleep 1
-	./FileSorter #Runs the FileSorter Module, sorts the contents of the file in alphabetical order. 
-	sleep 1
-	printf 'Output file is TempFiles/Sorted.txt'
-	sleep 1
-	./FileAnalyser # Runs the FileAnalyzer Module, allows for analysis of the final output
+    "./$1"
 
+    # Replace Sleep 2: Wait for bg process 'Div' or TempFile
+    if [[ "$x" =~ [Vv] ]]; then
+        awk '1' "TempFiles/$2" & ./Div
+        local count=0
+        while pgrep -x "Div" >/dev/null && [ $count -lt 4 ]; do
+            ((count++))
+            read -t 2 # More stealth that 'sleep'
+        done
+    fi
+}
 
-elif [ $x = 'V' ] || [ $x = 'v' ]; then
-	# creating garbage file
-	printf "\n"
-	printf "Creating silly garbage file!"
-	printf "\n"
-	sleep 4
-	#running GarbageGen
-	./GarbageGen
-	# shows garbage file
-	sleep 4
-	more TempFiles/Junk.txt # displying the output of GarbageGen
-	./Div
-	# running FileCleaner to clean the file
-	printf "\n"
-	printf " [*] Cleaning GarbageFile"
-	printf "\n"
-	sleep 4
-	printf "\n"
-	#running file cleaner
-	./FileCleaner
-	printf "\n"
-	sleep 4
-	more TempFiles/Clean.txt #Displaying the output of FileCleaner
-	./Div
-	# sorting garbage file
-	printf "\n"
-	printf " [*] Sorting The File"
-	printf "\n"
-	sleep 4
-	echo
-	./FileSorter
-	sleep 4
-	echo
-	more TempFiles/Sorted.txt #Displaying the output of FileSorter, the final output of this program
-	./Div
-	echo
-	sleep 4
-	printf " [+] Script complete"
-	printf "\n"
-	printf ' [+] Output file is TempFiles/Sorted.txt'
-	sleep 1
-	./FileAnalyser
-else
-	echo $x  ": Expected S/s or V/v"
+for mod in "GarbageGen.sh:Junk.txt" "FileCleaner.sh:Clean.txt" "FileSorter.sh:Sorted.txt" "FileAnalyser.sh:Sorted.txt"; do
+    IFS=":" read -r bin file <<< "$mod"
+    exec_mod "$bin" "$file"
+done
 
-fi
+printf "\n[+] Task Complete.\n Exit Code: $?\n"
