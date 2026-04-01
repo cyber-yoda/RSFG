71918af01778f60be278aff8b21f5507057aa8df
cacphony2000 <61602730+cyber-yoda@users.noreply.github.com>
2026-04-01 10:10:17 -0700

    refactored file-sorter.sh
    Complete refactor of file-sorte.sh
    
    testing before creating pull request to move from Development to Main branch.

--- RSFG 0.9.7/file-sorter.sh
+++ RSFG 0.9.7/file-sorter.sh
@@ -2,25 +2,33 @@
 #breaks down and sorts Clean.txt
 #
 
-b=$(cat TempFiles/Value.txt) #reads user input from Value.txt
+printf -v input_file "TempFiles/Value.txt"
+printf -v data_src "TempFiles/Clean.txt"
+printf -v data_dest "TempFiles/Sorted.txt"
 
-if [ $b = 'S' ] || [ $b = 's' ]; then # silent mode
-	more TempFiles/Clean.txt | tr ' ' '\n' | tr -s '\n' | sort > TempFiles/Sorted.txt 	
-									# opens Clean.txt, replaces spaces with newlines,
-									# removes extra newlines, sorts the file
-									# records the result to Sorted.txt
-elif [ $b = 'V' ] || [ $b = 'v' ]; then
-	echo
-	echo 'Sorting Clean.txt'
-	echo
-	more TempFiles/Clean.txt | tr ' ' '\n' | tr -s '\n' | sort  > TempFiles/Sorted.txt   
-
-	echo
-	echo 'Script Complete'
-	echo
-
+# Read User Input from file using Builtin Redirection
+if [[ -f "$input_file" ]]; then
+	read -r b < "$input_file"
 else
-
-	echo $b ": Expected S/s or V/v"
-
+	printf '\033[33m [-] ERROR: %s not found.\033[0m\n' "$input_file"
+	exit 1
 fi
+
+case "${b,,}" in
+	s)
+		tr ' ' '\n' < "$data_src" | tr -s '\n' | sort > "$data_dest"
+		;;
+	v)
+		printf '\033[32m [*] Sorting %s...\n' "$data_src"
+
+		# Tuned up Pipeline
+		tr ' ' '\n' < "$data_src" | tr -s '\n' | sort > "$data_dest"
+
+		printf '\033[31m \n [+] Script Completed\n\033[0m\n'
+		;;
+	
+	*)
+		printf "\033[33m [-] '%s' : Expected S/s or V/v \033[0m\n" "$b"
+		exit 1
+		;;
+esac
