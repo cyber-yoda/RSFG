c516ce041676b99ae62be2024704ca5da1838581
cacphony2000 <61602730+cyber-yoda@users.noreply.github.com>
2026-04-01 09:39:23 -0700

    Update garbage-gen.sh
    Complete refactor of garbage-gen.sh
    
    Testing before creating pull request to merge from Development to Main branch

--- RSFG 0.9.7/garbage-gen.sh
+++ RSFG 0.9.7/garbage-gen.sh
@@ -1,42 +1,60 @@
 #!/usr/bin/env bash
 #generates a file full of random junk
 #
-# set envrioment var from Value.txt
+# set environment variable from Value.txt
 
-a=$(cat TempFiles/Value.txt) # reads user input
+printf -v source_file "TempFiles/Value.txt"
+printf -v output_file "TempFiles/Junk.txt"
 
-if [ $a = 'S' ] || [ $a = 's' ]; then # silent mode
-	touch TempFiles/Junk.txt # creates Junk.txt if it does not exist.
-	for a in {1..9}
-	do
-	#echo >> TempFiles/Junk.txt
-	./RandomGen >> TempFiles/Junk.txt # runs the random gen command 9 times and appends the results to Junk.txt
-	echo >> TempFiles/Junk.txt
-	done
-elif [ $a = 'V' ] || [ $a = 'v' ]; then
-	echo
-	echo 'Creating an empty file: Junk.txt, or updating the time stamp if the file already exists...'
-	echo
-	touch TempFiles/Junk.txt
-	echo
-	echo 'Appending junk to file...'
-	echo
-	#starting for loop
-	for a in {1..9}
-	do
-		#echo >> TempFiles/Junk.txt
-		./RandomGen >> TempFiles/Junk.txt
-		echo >> TempFiles/Junk.txt
-	done
-	echo
-	echo 'Script complete'
-	echo
+[[ -d "TempFiles" ]] || mkdir -p TempFiles
 
+# Check that 'RandomGen' exists and can Execute
+if [[ -f "./RandomGen" ]]; then
+	[[ -x "./RandomGen" ]] || chmod +x ./RandomGen
 else
-
-	echo $a ": Expected S/s or V/v"
-
+	printf '\033[33m [!] ERROR - RandomGen not Found.\033[0m\n'
+	exit 1
 fi
-	
 
+# Check that 'Value.txt' exists
+if [[ ! -f "$source_file" ]]; then
+	printf '\033[33m [!] ERROR - %s is Missing.\033[0m' "$source_file"
+	exit 1
+fi
 
+# User input on Mode
+printf ' [*] Select Output Mode ( [S]ilent / [V]erbose ) > '
+read -r mode
+
+# Execution
+case "${mode,,}" in
+	s|silent)
+		touch "$outputfile"
+		for (( i=1; i<=9; i++ )); do
+			# Append RandomGen to $outpuf_file
+			./RandomGen >> "$output_file"
+			# Append Garbage Data from Value.txt
+			prinf "\n" >> "$outputfile"
+			cat "$source_file" >> "$output_file"
+			printf "\n" >> "$output_file"
+		done
+		;;
+	v|verbose)
+		printf '\033[31m [*] Initializing %s... \033[0m\n' "$output_file"
+		touch "$output_file"
+
+		printf '\033[31, [*] Processing 9 iterations of Junk Generation... \n'
+
+		for (( i=1; i<=9; i++ )); do
+			./RandomGen >> "$output_file"
+			printf "\n" >> "$output_file"
+			cat "$source_file" >> "$output_file"
+			printf "\n" >> "$output_file"
+			printf '\033[31m [+] Iteration %d Complete (RandomGen + Value.txt Data Added)\033[0m\n' "$i"
+		done
+
+		printf '\033[31m [+] Script Complete. Check %s for Results.\n' "$output_file"
+		;;
+	*)
+		printf "\033[33m [-] '%s' is not a Valid Mode! Please us S or V \033[0m\n" "$mode"
+esac
