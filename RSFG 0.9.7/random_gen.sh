e0e2c79f49149fdd0a915feaa78e540663f68dde
cacphony2000 <61602730+cyber-yoda@users.noreply.github.com>
2026-04-01 10:35:18 -0700

    Update random_gen.sh
    Refactor/Refinement of variable declaration, clean_up, and processing using awk.
    
    Testing before creating pull request to move from Development to Main branch

--- RSFG 0.9.7/random_gen.sh
+++ RSFG 0.9.7/random_gen.sh
@@ -3,23 +3,41 @@
 #
 # Name: random_gen.sh
 # Project: RSFG
-# Description: Factor of majority and generate a random string using with awk; collapse sed and tr since awk can do what they do
+# Description: Generate a random string using with awk; use bash builtins to cleanup with awk for tthe Fish-Yates Shuffle
 #
 
 generator() {
   # lists and arrays (wl just means wordlist)
-  wl='{Billy, Duran, Brenda, Dragonfruit, Apple, Bananna, Grape} {Apple, Cheese, Potato, Pork, Bread, Butter, Beans} {Keyboard, Computer, Mouse, CPU, RAM, HDD, GPU} {Doggy, Flamingo, Frog, Kitten, Bird, Eagle, Capybara} {Autism, Epilepsy, PTSD, Depression} {Motherboard, Chipset, Periphral Component Interconnect, Pin Grid Array, Land Grid Array}'
+  local wl
+  printf -v wl '%s' '{Billy, Duran, Brenda, Dragonfruit, Apple, Bananna, Grape} {Apple, Cheese, Potato, Pork, Bread, Butter, Beans} {Keyboard, Computer, Mouse, CPU, RAM, HDD, GPU} {Doggy, Flamingo, Frog, Kitten, Bird, Eagle, Capybara} {OCD, NeuroDivergence, Epilepsy, PTSD, Depression} {Motherboard, North Bridge, South Bridge, Universal Serial Bus, Periphral Component Interconnect, Pin Grid Array, Land Grid Array}'
 
-  # Use printf instead of echo, sed to clean it up, tr it into newline-separated words, and awk 6 lines
-  printf '%s' "$wl" | awk '
-    BEGIN { srand(); RS="[{} ,]+" }
-    /[^[:space:]]/ { a[++n] = $0 } 
+  # clean up using paramter expansion
+  local clean_wl
+  printf -v clean_wl '%s' "{wl//[{}]/]"
+  
+  # Process with awk
+  printf '%s' "$clean_wl" | awk -v seed="$RANDOM" '
+    BEGIN {
+      srand();
+      FS=",[ ]*|[ ]+"
+    }
+    {
+      for (i=1; i<=NF; i++) {
+        if ($i != "") a[++n] = $i
+      }
+    }
     END {
+        # Fisher-Yates Shuffle
         for (i=n; i>1; i--) {
-            j = int(rand() * i) + 1; t=a[i]; a[i]=a[j]; a[j]=t
+          j = int(rand() * i) + 1
+          t = a[i]; a[i] = a[j]; a[j] = t
         }
-        for (k = 1; k <=6; k++) print a[k]
-    }'
+        # Print top 6 random items
+        for (k = 1; k <= 6; k++) {
+          printf "%s%s", a[k], (k == 6 ? "" : " ")
+        }
+        printf "\n"
+      }'
 }
 
 generator
