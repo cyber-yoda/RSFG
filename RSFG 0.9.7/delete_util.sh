851d7595420d30f1060385f5e3e3aed43bd7fd4c
cacphony2000 <61602730+cyber-yoda@users.noreply.github.com>
2026-04-01 11:12:51 -0700

    Update delete_util.sh
    Refactor of delete-util.sh
    
    testing before creating pull request to move from Development to Main

--- RSFG 0.9.7/delete_util.sh
+++ RSFG 0.9.7/delete_util.sh
@@ -6,26 +6,33 @@
 # Description: Simple script that will allow the user to delete the files in a fixed directory.
 #
 
-read -p "Delete .txt files Y/n > " user_input
+printf -v target "$%s/Projects/RSFG/TempFiles/deleteme.txt" "$HOME"
 
-target_file="$HOME/Projects/RSFG/TempFiles/deleteme.txt"
+printf "\033[31m Delete .txt files Y/n > \033[0m" user_input
+read -r user_input
 
-if [[ "$user_input" =~ ^[Yy]$ ]]; then
+# Take user input
+case "${user_input,,}" in
+	y|yes)
+		# Check existence and delete in one line
+		if [[ -f "$target" ]]; then
+			printf "\033[31m [+] Found file, removing: %s\033[0m\n..." "${target##*/}"
 
-    if [[ -f "$target_file" ]]; then
-        printf " [+] Found file, deleting... \n"
-        rm -Iv "$target_file"
-        printf " [+] File deleted successfully.\n"
-	
-    else
-        printf " [-] No such file: %s\n" "$target_file"
-	
-    fi
+			# rm is yes an external binary but -f prevent the script from clinging 
+			# to a confirmation prompt
+        	rm -rf "$target" && printf "\033[31m [+] Removal Successful.\033[0m\n" || printf "\033[33m Failed to Remove File.\033[0m\n"
+		else
+        	printf "\033[33m [-] Target File not found: %s\033[0m\n" "$target"
+		fi
+		;;
 
-elif [[ "$user_input" =~ ^[Nn]$ ]]; then
-    printf " [-] No files deleted... \n"
-
-else
-    printf "%s: [-] Expected: y or n\n" "$user_input"
-
-fi
+	n|no)
+		printf "\033[33m [-] Operation Aborted. No Files Deleted.\n"
+		;;
+		
+	*)
+		# Using %q in printf safely escape weird garbage character period.
+		printf '\033[33m [-] %q : Expected y/n\033[0m\n' "$user_input"
+		exit 1
+		;;
+esac
