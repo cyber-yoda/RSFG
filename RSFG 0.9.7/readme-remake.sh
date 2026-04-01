af220ed4821a92268e9fe1596f4013acb687fcd5
cacphony2000 <61602730+cyber-yoda@users.noreply.github.com>
2026-04-01 10:21:42 -0700

    refactor/overhaul to readme-remake.sh
    Refactor but more an overhaul of readme-remake.sh
    
    testing before creating a pull request to move from Development to Main branch

--- RSFG 0.9.7/readme-remake.sh
+++ RSFG 0.9.7/readme-remake.sh
@@ -2,28 +2,34 @@
 # more of a redundancy in the change the readme file
 # does not already exist
 
-awk 'BEGIN { system("touch README.md") }'
-markdown=$( awk '{ chars += length($0) +1 } END { print chars }' README.md | awk '{ gsub(/[[:alpha:]]/, ""); print }' | awk '{ gsub(/\./, ""); print }')
 
+# variable declaration using printf -v
+printf -v target_file "README.md"
 
-if [ "$markdown" -gt 2 ]; then
+# existence and size checking (uses builtins)
+[[ -f "$target_file" ]] || : > "$target_file"
 
-	printf '[!] README.txt exists \n'
+# -s to check if file exists and has a greater size than zero
+# builtin-like check via `wc -c`
+printf -v markdown "%d" "$(wc -c < "$target_file")"
 
+if (( markdown > 2 )); then
+	printf '\033[31m [+] %s Exists and contains Data.\033[0m\n' "$target_file"
 else
+	printf '\033[33m [*] Recreating missing file %s...\033[0m\n' "$target_file"
 
-	printf '[*] Recreating README.txt \n'
+	cat <<- 'EOF' > "$target_file"
+		Silly bash scripts made I have.
+		Program called 'RSFG'; Really-Silly-File-Generator it is.
+		Run you can, by opening this folder in Terminal and running:
 
-	printf "%s\n\n" "Silly bash scripts made I have.
-	Program called 'RSFG'; Really-Silly-File-Generator it is.
-	Run you can, by opening this folder in Terminal and running:
+			cd rsfg-refactor
+			chmod 700 rsfg
+			./rsfg
 
-		cd rsfg-refactor
-		chmod 700 rsfg
-		./rsfg
-
-	Have fun. Have strange open file errors, alter environment variable
-	at start of scripts to match your system be sound solution.
-	Look through codebase before run any, to know what it nature.
-	Smart to do for random stuff dowloaded off internet." > README.md
+		Have fun. Have strange open file errors, alter environment variable
+		at start of scripts to match your system be sound solution.
+		Look through codebase before run any, to know what it nature.
+		Smart to do for random stuff dowloaded off internet. > README.md
+		EOF
 fi
